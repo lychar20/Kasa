1e05bf0b5b086c06707121d2fdc875a963eece28
Charly Miampunina Mampuya <charly.mampuya@yahoo.fr>
2023-02-11 04:35:24 +0100

    iteration avec composant card ok

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+//import * as React from "react";
+import React,{useState,useEffect} from "react";
 import './home.scss'
 import Banner from 'components/banner/index'
 import IMG from 'assets/image/IMG.png'
@@ -8,18 +9,55 @@ import Card from "components/card";
 function Home() {
 
   const bannerTxt = "Chez vous partout et ailleurs"
-  const cardTxt = "Titre de la location "
 
-    return (
-      <div>
-        
-
-        <Banner img={IMG} txt={bannerTxt} />
-        <Card txt={cardTxt} />
-        
-        
-      </div>
+  const [data,setData]= useState([]);
+  const getData=()=> {
+    fetch('./logements.json'
+    ,{
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }
     )
+    .then(function(response){
+      console.log(response)
+      return response.json();
+    })
+    .then(function(myJson){
+      console.log(myJson);
+      setData(myJson)
+    });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+
+  return (
+      
+   
+    <div>
+    <Banner img={IMG} txt={bannerTxt} />
+    
+  
+
+<div className="Card">
+{data.map((appart, id) => (
+<div className="card_logement" key={id}>
+ {/* <Link className="link_card_logement" to={`/logement/${appart.id}`}> */}
+  <Card cover={appart.cover} title={appart.title} />
+{/* </Link>  */}
+</div>
+))}
+</div>
+</div>
+
+      
+  
+)
+
+ 
+  
   }
   
   export default Home
@@ -27,12 +65,29 @@ function Home() {
   
 
 
-/*   return (
-    <div>
-      
 
-      <Banner><img src={IMG} alt='montagne' className='montagne' /> <h1 className="txt">{bannerTxt}</h1>  </Banner>
-      
-      
-    </div>
-  ) */
+  //----------------------------------------test--------------------------------------------
+
+/*   function Home() {
+
+    const bannerTxt = "Chez vous partout et ailleurs"
+    const cardTxt = "Titre de la location "
+  
+      return (
+        <div>
+          
+  
+          <Banner img={IMG} txt={bannerTxt} />
+          <Card txt={cardTxt} />
+          
+          
+        </div>
+      )
+    } */
+
+
+
+    //------------------------------nouveu test
+
+
+    
